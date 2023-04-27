987f90c4542a0a25b99a05fb284f0ada4e7f93f2
Lunix <ramezjoseph8@gmail.com>
2023-04-27 21:18:11 +0200

    UI + Jumbotron

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,14 @@ import { Inter } from 'next/font/google'
 import React, { useState } from 'react';
 import { useSession, signIn, signOut } from "next-auth/react"
 import Navbar from "../Components/Navbar";
+import Jumbotron from '@/Components/Jumbotron';
 
 export default function Home() {
   return (
     <div class="h-full min-h-screen bg-[#090909]">
   <Navbar/>
+  <br></br>
+  <Jumbotron />
     </div>
   )
 }
