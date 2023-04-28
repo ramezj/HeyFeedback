fe3f97a3a56dc6645024ad703cca8c333ab002b6
Lunix <ramezjoseph8@gmail.com>
2023-04-28 20:26:25 +0200

    Indigo Colors, test

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button onClick={props.onClick} type="button" class="text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 focus:outline-none font-bold rounded-lg text-lg px-10 py-2.5 text-center mr-4 mt-4 shadow-lg shadow-teal-500/50">{props.title}</button>
+    <button onClick={props.onClick} type="button" class="shadow-md shadow-indigo-500/20 text-indigo-300 bg-indigo-900 focus:outline-none font-bold rounded-lg text-lg px-10 py-2.5 text-center mr-4 mt-4">{props.title}</button>
   )
 }
 
