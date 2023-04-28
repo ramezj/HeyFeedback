14e14325451d1603e074cfb2ef66977c7a101af8
Lunix <ramezjoseph8@gmail.com>
2023-04-28 20:31:39 +0200

    new colors

--- src/Components/Badge.js
+++ src/Components/Badge.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Badge = (props) => {
   return (
-    <span class="bg-indigo-100 text-indigo-800 text-md font-medium mr-2 px-5 py-2 rounded-full dark:bg-indigo-900 dark:text-indigo-300">{props.text}</span>
+    <span 
+    class="bg-indigo-100 
+    text-indigo-800 
+    text-md 
+    font-medium 
+    mr-2 
+    px-5 py-2 
+    rounded-full 
+    dark:bg-indigo-600 
+    dark:text-indigo-100
+    ">
+    {props.text}
+    </span>
   )
 }
 
--- src/Components/Button.js
+++ src/Components/Button.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button onClick={props.onClick} type="button" class="shadow-md shadow-indigo-500/20 text-indigo-300 bg-indigo-900 focus:outline-none font-bold rounded-lg text-lg px-10 py-2.5 text-center mr-4 mt-4">{props.title}</button>
+    <button 
+    onClick={props.onClick} 
+    type="button" 
+    class="shadow-md
+    shadow-indigo-500/20
+    text-indigo-100 
+    bg-indigo-600 
+    focus:outline-none 
+    font-bold 
+    rounded-lg 
+    text-lg 
+    px-10 py-2.5 
+    text-center 
+    mr-4 mt-4 
+    hover:bg-indigo-500 
+    duration-500">
+    {props.title}
+    </button>
   )
 }
 
