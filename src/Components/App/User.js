3c2a9aed4118c2abdf105f5cbc5dcb10c5d95061
Lunix <ramezjoseph8@gmail.com>
2023-05-01 10:50:50 +0200

    new fetch, new geo emoji

--- src/Components/App/User.js
+++ src/Components/App/User.js
@@ -1,14 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
 import useSWR from 'swr'
 import { motion } from 'framer-motion';
+import { useRouter } from 'next/router';
+import Badge from '../Badge';
+import ReactCountryFlag from "react-country-flag"
 
 const User = () => {
-  const fetcher = (...args) => fetch(...args).then(res => res.json());
-  const { data: session, status } = useSession()
-  const { data, error, isLoading } = useSWR('/api/User', fetcher)
-  if (error) return <div>failed to load</div>
-  if (isLoading) return (
+  const { data: session, status } = useSession({
+    // Redirect User if Unauthenticated
+    required:true,
+    onUnauthenticated() {
+      router.push('/')
+    }
+  })
+  const router = useRouter();
+  const [ data, setData ] = useState();
+  const [ loading, setLoading ] = useState()
+  const [ error, setError ] = useState()
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      const res = await fetch('/api/User');
+      const resJSON = await res.json();
+      setLoading(false);
+      if(data.ok == false) {
+        setError(resJSON);
+      }
+      setData(resJSON);
+    }
+
+    fetchData();
+  }, [])
+  if (loading) return (
     <>
     <div><span class="w-2 h-2 ml-2 rounded-full bg-gray-200 inline-block animate-flash"></span><span class="w-2 h-2 ml-2 rounded-full bg-gray-200 inline-block animate-flash [animation-delay:0.2s]"></span><span class="w-2 h-2 ml-2 rounded-full bg-gray-200 inline-block animate-flash [animation-delay:0.4s]"></span></div>
     <h1>Loading your amazing feedback</h1>
@@ -26,9 +50,13 @@ const User = () => {
         scale:1.1
     }}
     className="w-full max-w-sm p-8 bg-form rounded-lg shadow-lg dark:bg-gradient-to-br from-[#1d1d1d] to-[#0c0c0c] dark:border-gray-700">
+                <a href="#" class="bg-green-100 text-green-800 text-md font-bold inline-flex items-center px-2.5 rounded-md dark:bg-gray-700 dark:text-green-400 mb-2">
+                    {x.ipAddress}
+                </a>
+    <br></br>
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{x.email}</h5>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Body :{x.body}</p>
-          <p>IP :{x.ipAddress}</p>
+          <ReactCountryFlag countryCode={x.geo} />
           <p>GEO :{x.geo}</p>
           </motion.div>
           </>
