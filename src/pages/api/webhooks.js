b4f1681e5b33c4703a23cf53b2a4b1209b2d633e
Lunix <ramezjoseph8@gmail.com>
2023-05-11 01:50:59 +0200

    webhooks debug

--- src/pages/api/webhooks.js
+++ src/pages/api/webhooks.js
@@ -41,6 +41,37 @@ export default async function handler(req, res) {
     //     })
     //   }
     // }
+    if(req.body.subscription_created) {
+      try {
+        const updateUser = await prisma.user.update({
+          where: {
+            id:req.body.passthrough
+          },
+          data: {
+            isSubscribed:true,
+            subscription_id: req.body.subscription_id,
+            subscription_update_url:req.body.update_url,
+            subscription_cancel_url:req.body.cancel_url,
+          }
+        });
+        if(updateUser) {
+          return res.status(200).json({
+            ok:true,
+            data: "Subscribed Successfully!"
+          })
+        } else {
+          return res.status(400).json({
+            ok:false,
+            data: "Couldn't Upgrade Subscription"
+          })
+        }
+      } catch (error) {
+        return res.status(404).json({
+          ok:false,
+          data: "something went wrong..."
+        })
+      }
+    }
     console.log(req.body);
     res.status(202).json(req.body)
   }
