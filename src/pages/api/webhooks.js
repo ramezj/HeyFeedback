0433e49ca95c178bd379bd56d51a6bc6679b101b
Lunix <ramezjoseph8@gmail.com>
2023-05-11 01:46:16 +0200

    webhooks debug

--- src/pages/api/webhooks.js
+++ src/pages/api/webhooks.js
@@ -8,38 +8,40 @@ export default async function handler(req, res) {
         message:"Method Not Allowed"
       })
     }
-    if(req.body.alert_name == "subscription_created") {
-      // Upgrade user's payment status
-      try {
-        const updateUser = await prisma.user.update({
-          where: {
-            id:req.body.passthrough
-          },
-          data:{
-            isSubscribed:true,
-            subscription_id: req.body.subscription_id,
-            subscription_update_url:req.body.update_url,
-            subscription_cancel_url:req.body.cancel_url,
-          }
-        });
-        if(updateUser) {
-          return res.status(200).json({
-            ok:true,
-            message:'Subscribed Successfully.'
-          })
-        } else {
-          return res.status(400).json({
-            ok:false,
-            message:"Couldn't Subscribe User"
-          })
-        }
-      } catch (error) {
-        return res.status(404).json({
-          ok:false,
-          message:'Something went completely wrong',
-          error:error
-        })
-      }
-    }
+    // if(req.body.alert_name == "subscription_created") {
+    //   // Upgrade user's payment status
+    //   try {
+    //     const updateUser = await prisma.user.update({
+    //       where: {
+    //         id:req.body.passthrough
+    //       },
+    //       data:{
+    //         isSubscribed:true,
+    //         subscription_id: req.body.subscription_id,
+    //         subscription_update_url:req.body.update_url,
+    //         subscription_cancel_url:req.body.cancel_url,
+    //       }
+    //     });
+    //     if(updateUser) {
+    //       return res.status(200).json({
+    //         ok:true,
+    //         message:'Subscribed Successfully.'
+    //       })
+    //     } else {
+    //       return res.status(400).json({
+    //         ok:false,
+    //         message:"Couldn't Subscribe User"
+    //       })
+    //     }
+    //   } catch (error) {
+    //     return res.status(404).json({
+    //       ok:false,
+    //       message:'Something went completely wrong',
+    //       error:error
+    //     })
+    //   }
+    // }
+    console.log(req.body);
+    res.status(202).json(req.body)
   }
   
