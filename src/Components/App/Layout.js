d40fcb3e40cf3eaebacb0925bcf7de691f1ddd13
Lunix <ramezjoseph8@gmail.com>
2023-05-17 17:12:49 +0200

    🚑 Layout Premium button update

--- src/Components/App/Layout.js
+++ src/Components/App/Layout.js
@@ -48,9 +48,9 @@ const Layout = () => {
             {
                premium 
                ? <>
-               <a class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#0e0e0e] dark:hover:bg-[#0e0e0e]">
+               <a class="cursor-pointer flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-[#0e0e0e] dark:hover:bg-[#0e0e0e]">
                <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-               <span class="flex-1 ml-3 whitespace-nowrap">Premium</span>
+               <span class="flex-1 ml-3 whitespace-nowrap cursor-pointer">Premium</span>
                </a>
                </>
                : <>
